9c42a96949bcaa07177b549e33f214a1278bd314
Artur Sadov <Art.S.V.86@yandex.ru>
2021-06-15 08:52:14 +0400

    Added files

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void shouldTestAllFieldsCorrectly() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иван");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+    }
+}
+
+
